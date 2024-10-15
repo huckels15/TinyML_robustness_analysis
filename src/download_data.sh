9aaef623628380e332e3d17b96be43c927ffc257
Jacob Huckelberry <69861603+huckels15@users.noreply.github.com>
2024-10-15 11:19:08 -0400

    Delete src/download_data.sh

--- src/download_data.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-wget https://www.silabs.com/public/files/github/machine_learning/benchmarks/datasets/vw_coco2014_96.tar.gz
-tar -xvf vw_coco2014_96.tar.gz
-
-wget https://www.cs.toronto.edu/~kriz/cifar-10-python.tar.gz
-tar -xvf cifar-10-python.tar.gz
