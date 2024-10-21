a18fd0d77b67804967d737b59a443361b21a335b
huckels15 <jacobhuckelbery@gmail.com>
2024-10-21 13:32:43 -0400

    Added knockoffnets

---
+++ src/robustness_testing_pipeline/attacks/run_knockoff_nets_cifar.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+DATASET_ID=cifar10
+DATASET_SIZE=1000
+NUM_CLASSES=10
+#ANN_FLOAT='models/cifar_resnet_float.h5'
+#ANN_FLOAT='models-defense_enhanced/cifar_adv_float.h5'
+#ANN_FLOAT='models-defense_enhanced/cifar_distil_float.h5'
+#ANN_FLOAT='models-defense_enhanced/cifar_ens_adv_float.h5'
+#ANN_FLOAT='models-defense_enhanced/cifar_sat_float.h5'
+#QNN_INT16='models/cifar_resnet_int16.tflite'
+#QNN_INT16='models-defense_enhanced/cifar_adv_int16.tflite'
+#QNN_INT16='models-defense_enhanced/cifar_distil_int16.tflite'
+#QNN_INT16='models-defense_enhanced/cifar_ens_adv_int16.tflite'
+#QNN_INT16='models-defense_enhanced/cifar_sat_int16.tflite'
+
+QNN_INT8='models/trainedResnet_20241021_1325_logits_quant.tflite'
+
+#QNN_INT8='models-defense_enhanced/cifar_adv_int8.tflite'
+#QNN_INT8='models-defense_enhanced/cifar_distil_int8.tflite'
+#QNN_INT8='models-defense_enhanced/cifar_ens_adv_int8.tflite'
+#QNN_INT8='models-defense_enhanced/cifar_sat_int8.tflite'
+
+echo "=============================================================="
+echo "======================KnockoffNets=============================="
+python3 knockoff_nets_int8.py --dataset_id "$DATASET_ID" --num_classes "$NUM_CLASSES" --target_int8 "$QNN_INT8"
